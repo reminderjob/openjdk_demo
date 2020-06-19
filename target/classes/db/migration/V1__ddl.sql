034d2929e79bf60266739023b900c4259388d846
EC2 Default User <ec2-user@ip-172-31-45-170.ap-southeast-1.compute.internal>
2020-06-19 10:33:49 +0000

    openjdk

--- target/classes/db/migration/V1__ddl.sql
+++
@@ -1,1 +0,0 @@
-create table users (id int8 not null, created_at timestamp not null, created_by varchar(255) not null, email_address varchar(255) not null, first_name varchar(255) not null, last_name varchar(255) not null, updated_at timestamp not null, updated_by varchar(255) not null, primary key (id))
